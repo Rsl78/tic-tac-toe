c0040169d517be06b54e0189be936de1e78c4da5
rsl78 <roknuzzaman.rasel78@gmail.com>
2024-10-08 18:29:27 +0600

    Board complete

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,17 @@
-export default function Square({ value }) {
+import PropTypes from "prop-types";
+
+export default function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-xl">
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-xl"
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
+}
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onSquareClick: PropTypes.func,
 }
